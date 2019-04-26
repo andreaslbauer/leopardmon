77d6b6d7d5910515b54d11c8abb4d6542e573231
Andreas L Bauer <andreaslbauer@gmail.com>
2019-04-26 16:46:18 -0400

    Read username and password from file

--- mailhelper/mailhelper.py
+++ mailhelper/mailhelper.py
@@ -1,9 +1,25 @@
 import smtplib
 
+# set up username and smtppassword as global variables
+global smtpusername
+smtpusername = None
+global smtppassword
+smtppassword = None
+
+# read the username (e-mail address) and password from file
+def readUsernamePassword() :
+    file = open("username.txt", "r")
+    line = file.readline()
+    splitLine = line.split()
+    global smtpusername
+    smtpusername = splitLine[0]
+    global smtppassword
+    smtppassword = splitLine[1]
+
 def sendMail(toAddresses: object, subject: object, body: object) -> object:
     fromAddress = 'webproprtiesmonitor@cellsignal.com'
-    username = 'andreaslbauer@gmail.com'
-    password = '384Robbi'
+    username = smtpusername
+    password = smtppassword
     server = smtplib.SMTP('smtp.gmail.com:587')
     server.ehlo()
     server.starttls()
