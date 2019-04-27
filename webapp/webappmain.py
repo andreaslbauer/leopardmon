a99441928f14ca6103440d27a36f48e353943dfa
Andreas L Bauer <andreaslbauer@gmail.com>
2019-04-27 19:45:33 -0400

    Implement dashboard page

--- webapp/webappmain.py
+++ webapp/webappmain.py
@@ -1,12 +1,74 @@
-from flask import Flask
+from flask import Flask, render_template
 import logging
 import os
 from . import auth
+import pycouchdb
+
+
+class TestResultList:
+
+    # default constructor- create empty object
+    def __init__(self):
+        self.searchResults = list()
+
+    # get our length
+    @property
+    def len(self):
+        return self.searchResults.__len__()
+
+    # load the target info records from the CouchDB
+    def loadFromDB(self) -> object:
+
+        # get the database
+        couchDBServer = pycouchdb.Server()
+        couchDB = couchDBServer.database("leopardmon-testresults")
+        _viewDoc = None
+
+        try:
+            _viewDoc = couchDB.get("_design/listview")
+
+        except pycouchdb.exceptions.NotFound as e:
+
+            # view does exist - create it
+            _viewdoc = {
+                "_id": "_design/listview",
+                "views": {
+                    "names": {
+                        "map": "function(doc) { emit(doc.name, 1); }",
+                        "reduce": "function(k, v) { return  sum(v); }",
+                    }
+                }
+            }
+
+            viewdoc = couchDB.save(_viewdoc)
+
+        # use the view to list all documents
+        docnames = couchDB.query("listview/names", group='true')
+
+        # load the targetinfo objects
+        for docname in docnames:
+            doc = couchDB.get(docname["key"])
+            testResults = doc
+            print(doc)
+            self.searchResults.append(doc)
+
+
+
+def getTestResults():
+    # create the couchdb instance - if it doesn't exist yet
+
+    testResultList = TestResultList()
+    testResultList.loadFromDB()
+
+    logging.info("Loading test results - got %i records", testResultList.len)
+
+    return testResultList.searchResults
+
 
 def createapp(test_config = None):
     # set up the logger
-    # logging.basicConfig(filename = "leopardapp.log", format='%(asctime)s %(levelname)s %(message)s',
-    #                    level=logging.INFO)
+    logging.basicConfig(filename = "leopardapp.log", format='%(asctime)s %(levelname)s %(message)s',
+                        level=logging.INFO)
 
     logging.info("Setting up Flask web app")
 
@@ -36,7 +98,24 @@ def createapp(test_config = None):
     @app.route('/')
     def home():
         logging.info("HTTP GET for /")
-        return 'Homepage!'
+        return render_template('home.html')
+
+    @app.route('/dashboard')
+    def dashboard():
+        logging.info("HTTP GET for /dashboard")
+
+        testResults = getTestResults()
+        return render_template('dashboard.html', testResults = testResults)
+
+    @app.route('/monitorlog')
+    def monitorlog():
+        logging.info("HTTP GET for /monitorlog")
+
+        file = open("leopardmon.log", "r")
+        lines = file.read()
+
+        return render_template('monitorlog.html', lines=lines)
+
 
     app.register_blueprint(auth.bp)
 
